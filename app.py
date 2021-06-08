f9b51412adb4c7308836eda88eba465da2ab5405
LoothTooth312 <84528439+LoothTooth312@users.noreply.github.com>
2021-06-08 22:52:59 +0100

    section 2 finished
    now onto 3

--- app.py
+++ app.py
@@ -12,6 +12,121 @@ BUGGY_RACE_SERVER_URL = "https://rhul.buggyrace.net"
 #------------------------------------------------------------
 # the index page
 #------------------------------------------------------------
+
+def costcalc(ppower,punit,apower,aunit,hamster,tyre,tyreunit,armour,attack,attackunit,fireproof,insulated,antibiotic,banging,):
+    cost = 0
+    if ppower == "petrol":
+        ppcost = 4
+    elif ppower == "fusion":
+        ppcost = 400
+    elif ppower == "steam":
+        ppcost = 3
+    elif ppower == "bio":
+        ppcost = 5
+    elif ppower == "electric":
+        ppcost = 20
+    elif ppower == "rocket":
+        ppcost = 16
+    elif ppower == "hamster":
+        ppcost = 2
+    elif ppower == "thermo":
+        ppcost = 300
+    elif ppower == "solar":
+        ppcost = 40
+    elif ppower == "wind":
+        ppcost = 20
+
+    ppcost = ppcost * int(punit) 
+    
+    if apower == "petrol":
+        apcost = 4
+    elif apower == "fusion":
+        apcost = 400
+    elif apower == "steam":
+        apcost = 3
+    elif apower == "bio":
+        apcost = 5
+    elif apower == "electric":
+        apcost = 20
+    elif apower == "rocket":
+        apcost = 16
+    elif apower == "hamster":
+        apcost = 2
+    elif apower == "thermo":
+        apcost = 300
+    elif apower == "solar":
+        apcost = 40
+    elif apower == "wind":
+        apcost = 20
+
+    hamstercost = int(hamster) * 5
+    apcost = apcost * int(aunit) 
+
+    powercost = apcost + ppcost + hamstercost
+
+    if tyre == "knobbly":
+        tcost = 15
+    elif tyre == "slick":
+        tcost = 10
+    elif tyre == "steelband":
+        tcost = 20
+    elif tyre == "reactive":
+        tcost = 40
+    elif tyre == "maglev":
+        tcost = 50
+
+    tcost = tcost * int(tyreunit)
+
+    if armour == "none":
+        armourcost = 0
+    elif armour == "wood":
+        armourcost = 40
+    elif armour == "aluminium":
+        armourcost = 200
+    elif armour == "thinsteel":
+        armourcost = 100
+    elif armour == "thicksteel":
+        armourcost = 200
+    elif armour == "titanium":
+        armourcost = 290
+
+    if attack == "none":
+        attackcost = 0 
+    elif attack == "spike":
+        attackcost = 5 
+    elif attack == "flame":
+        attackcost = 20 
+    elif attack == "charge":
+        attackcost = 28
+    elif attack == "biohazard":
+        attackcost = 30 
+
+    attackcost = attackcost * int(attackunit)
+
+    if fireproof == "1":
+        fireproofcost = 70
+    else:
+        fireproofcost = 0
+    if insulated == "1":
+        insulatedcost = 100
+    else:
+        insulatedcost = 0
+    if antibiotic == "1":
+        antibioticcost = 90
+    else:
+        antibioticcost = 0
+    if banging == "1":
+        bangingcost = 42
+    else:
+        bangingcost = 0
+        
+
+    totalcost = powercost + tcost +armourcost + attackcost + fireproofcost + insulatedcost + antibioticcost + bangingcost
+    
+    return totalcost
+
+
+
 @app.route('/')
 def home():
     return render_template('index.html', server_url=BUGGY_RACE_SERVER_URL)
@@ -24,31 +139,103 @@ def home():
 @app.route('/new', methods = ['POST', 'GET'])
 def create_buggy():
     if request.method == 'GET':
-        return render_template("buggy-form.html")
+       con = sql.connect(DATABASE_FILE)
+       con.row_factory = sql.Row
+       cur = con.cursor()
+       cur.execute("SELECT * FROM buggies")
+       record = cur.fetchone(); 
+       return render_template("buggy-form.html" , buggy = record)
     elif request.method == 'POST':
-        msg=""
+        msg = ''
+        vio = ''
         qty_wheels = request.form['qty_wheels']
         if not qty_wheels.isdigit():
-            msg = f"The input for Number of wheels must be a number"
-            return render_template("buggy-form.html", msg = msg)
+            msg = "The number of wheels must be a number"
+            return render_template("updated.html" , msg = msg)
+        elif int(qty_wheels) < 4:
+            msg = "The number of wheels must be greater than four"
+            return render_template("updated.html" , msg = msg)
+        elif (int(qty_wheels) % 2) != 0:
+            vio = "RULE VIOLATION: Please enter in an even number of wheels"
         flag_color = request.form['flag_color']
+        flag_color_secondary = request.form['flag_color_secondary']
+        flag_pattern = request.form['flag_pattern']
+        if flag_color_secondary.lower() == flag_color.lower() and flag_pattern != "plain":
+            vio = "RULE VIOLATION: Please enter in a different secondary colour "
+        power_type = request.form['power_type']
+        power_units = request.form['power_units']
+        if not power_units.isdigit():
+            msg = "The input for units of power must be a number"
+            return render_template("updated.html" , msg = msg)
+        if int(power_units) < 1:
+            msg = "The primary power must be greater than 1"
+            return render_template("updated.html" , msg = msg)
+        aux_power_type = request.form['aux_power_type']
+        aux_power_unit = request.form['aux_power_unit']
+        hamster_booster = request.form['hamster_booster']
+        if not power_units.isdigit():
+            msg = "The input for number of hamster boosters must be a number"
+            return render_template("updated.html" , msg = msg)
+        elif power_type != 'hamster' and int(hamster_booster) > 0 or aux_power_type != 'hamster' and int(hamster_booster) > 0:
+            msg = "A power type must be set to hamster in order to use this"
+            return render_template("updated.html" , msg = msg)
+        tyres = request.form['tyres']
+        qty_tyres = request.form['qty_tyres']
+        if int(qty_tyres) < int(qty_wheels):
+            vio = "RULE VIOLATION: The number of tyres can not be less than the number of wheels "
+            return render_template("updated.html" , msg = msg)
+        armour = request.form['armour']
+        attack = request.form['attack']
+        qty_attacks = request.form['qty_attacks']
+        try:
+            fireproof = request.form['fireproof']
+        except:
+            fireproof = False
+        try:
+            insulated = request.form['insulated']
+        except:
+            insulated = False
+        try: 
+            antibiotic = request.form['antibiotic']
+        except:
+            antibiotic = False 
+        try:
+            banging = request.form['banging']
+        except:
+            banging = False
+        algo = request.form['algo']
+        if algo == "buggy":
+            vio = "RULE VIOLATION: the algorithm can not be buggy"
+        total_cost = costcalc(power_type,power_units,aux_power_type,aux_power_unit,hamster_booster,tyres,qty_tyres,armour,attack,qty_attacks,fireproof,insulated,antibiotic,banging)
         try:
             with sql.connect(DATABASE_FILE) as con:
                 cur = con.cursor()
                 cur.execute(
-                    "UPDATE buggies set qty_wheels=?, flag_color=? WHERE id=?",
-                    (qty_wheels, flag_color, DEFAULT_BUGGY_ID),
+                    "UPDATE buggies set qty_wheels=?, flag_color=?, flag_color_secondary=?, flag_pattern=? WHERE id=?",
+                    (qty_wheels, flag_color, flag_color_secondary, flag_pattern, DEFAULT_BUGGY_ID),
+                )
+                cur.execute(
+                    "UPDATE buggies set power_type=?, power_units=?, aux_power_type=?, aux_power_unit=?, hamster_booster=? WHERE id=?",
+                    (power_type,power_units,aux_power_type,aux_power_unit,hamster_booster, DEFAULT_BUGGY_ID )
+                )
+                cur.execute(
+                    "UPDATE buggies set tyres=?, qty_tyres=? WHERE id=?",
+                    (tyres, qty_tyres, DEFAULT_BUGGY_ID),
+                )
+                cur.execute(
+                    "UPDATE buggies set armour=?, attack=?,qty_attacks=?, fireproof=?, insulated=?, antibiotic=?,banging=?, algo=? WHERE id=?",
+                    (armour, attack, qty_attacks, fireproof, insulated, antibiotic, banging, algo, DEFAULT_BUGGY_ID),
                 )
                 con.commit()
-                #msg = "Record successfully saved"
-                msg = f"qty_wheels={qty_wheels}" ,f"flag_color={flag_color}"
+                msg = f"Record successfully saved, the total cost is {total_cost}" 
+                #msg = f"qty_wheels={qty_wheels}" ,f"flag_color={flag_color}"S
 
         except:
             con.rollback()
             msg = "error in update operation"
         finally:
             con.close()
-        return render_template("updated.html", msg = msg)
+        return render_template("updated.html", msg = msg, vio = vio)
 
 #------------------------------------------------------------
 # a page for displaying the buggy
@@ -60,7 +247,7 @@ def show_buggies():
     cur = con.cursor()
     cur.execute("SELECT * FROM buggies")
     record = cur.fetchone(); 
-    return render_template("buggy.html", buggy = record)
+    return render_template("buggy.html" , buggy = record)
 
 #------------------------------------------------------------
 # a placeholder page for editing the buggy: you'll need
